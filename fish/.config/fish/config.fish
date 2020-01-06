7a6790753ecbd4a1e38411eae95b5b063fe7f508
Jack Arthur Harrhy <me@jackharrhy.com>
2020-01-06 01:13:54 -0330

    ensure fisher installed, diff is SO FANCY

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 set EDITOR vim
 
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 function op-getpass
   op get item $argv[1] | jq '.details.fields[] | select(.designation=="password").value'
 end
