af8555c5be548b67cd10e319d6fe6b8318c3f082
Jack Arthur Harrhy <me@jackharrhy.com>
2020-02-18 11:05:56 -0330

    munmon

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -41,6 +41,11 @@ function mun
   sudo netctl start mun-guest
 end
 
+function munmon
+  echo "1920x1080 59.96 Hz (CVT 2.07M9) hsync: 67.16 kHz; pclk: 173.00 MHz"
+  xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+end
+
 function mybar
   while true
     acpi
