d4d628ede56db8d503dbad74bd7cce6e1232212d
Jack Arthur Harrhy <me@jackharrhy.com>
2020-05-08 09:38:35 -0230

    pyenv init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 set EDITOR vim
 
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
