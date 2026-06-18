59c1a1bc576df338dd8993a142e4480b1adf6917
Jack Arthur Harrhy <me@jackharrhy.com>
2026-06-18 01:06:56 -0230

    remove fisher

--- macos/run_once_after_fish-plugins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-command -v fish >/dev/null 2>&1 || exit 0
-fish -c 'if not functions -q fisher; set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config; curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish; end; fisher update'
